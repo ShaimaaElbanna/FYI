edeacf4191e2fa1de2e2764615ce8f6a747f5584
ShaimaaElbanna <ShaimaaElbanna@users.noreply.github.com>
2018-10-19 16:45:25 +0200

    ok

--- While.js
+++ While.js
@@ -40,13 +40,80 @@ function lookUpProfile(name, prop){
         }
     } 
 }
-    else {
+ 
       return "No such contact";
     
-  }
+
 
 // Only change code above this line
 }
 // Change these values to test your function
 lookUpProfile("Akira", "likes");
 lookUpProfile("Kristian", "number");
+
+
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+ var answer = ""; 
+switch(card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  count = count + 1;
+  break;
+  case 7:
+  case 8:
+  case 9:
+  count = count + 0;
+  break;
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+  count = count -1;
+}
+if (count > 0){
+  return count + " Bet"
+} else if (count <= 0){
+  return count + " Hold"
+}
+
+  
+  return count;
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');  
+
+
+
+
+
+// Setup
+function phoneticLookup(val) {
+    var result = "";
+  
+    // Only change code below this line
+  var lookup = {
+      "alpha"   :  "Adams",
+      "bravo"   :  "Boston",
+      "charlie" :  "Chicago",
+      "delta"   :  "Denver",
+      "echo"    :  "Easy",
+      "foxtrot" :  "Frank",      
+    }
+    result = lookup[val]
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  phoneticLookup("charlie");
